6342703e4672d4edc31f8ccdde44fc74b3ecdc69
nutcha123 <nutcha.ptc@gmail.com>
2020-09-17 10:58:28 +0700

    commit5

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    //flexDirection: 'row',
     justifyContent: 'space-around',
     borderWidth: 5,
     borderColor: 'yellow',    
