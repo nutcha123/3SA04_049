8d5755aee7d8aedf7b267423d3a5fc875456a83d
nutcha123 <nutcha.ptc@gmail.com>
2020-09-17 11:32:12 +0700

    commit6

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     //flexDirection: 'row',
-    justifyContent: 'space-around',
+    //justifyContent: 'space-around',
     borderWidth: 5,
     borderColor: 'yellow',    
   }
